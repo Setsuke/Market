a8a92bffc3139157d68917629e6b34655e20988d
Setsuke <sergio_alva99@hotmail.com>
2021-12-04 01:01:37 -0500

    feat: entities

---
+++ src/test/java/com/setsuke/market/SetsukeMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.setsuke.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetsukeMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
